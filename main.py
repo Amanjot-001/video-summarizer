6284a0cba29ccf6534ee5ed2772226498f181a57
Amanjot-001 <amanjotsingh2309@gmail.com>
2025-06-23 13:04:01 +0530

    add: testing logic

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from transcription.whisper_transcriber import transcribe_audio, save_transcript
 from video.downloader import download_youtube_audio
 
-video_url = "https://www.youtube.com/watch?v=EQsQeBsB6YI"
+# video_url = "https://www.youtube.com/watch?v=EQsQeBsB6YI"
+video_url = "https://www.youtube.com/watch?v=QUTYxwTsbiM"
 
 if __name__ == "__main__":
     youtube_url = video_url
     audio_path, title = download_youtube_audio(youtube_url)
+
+    transcript, segments, language = transcribe_audio(audio_path, model_size="tiny")
+    save_transcript(audio_path, transcript, segments, language)
+    
